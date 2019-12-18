cde33d59465c37aa3f966b37af4997806f909abf
gebeto <slavik.nychkalo@gmail.com>
2019-12-18 17:56:24 +0200

    improve lab3

--- security/lab3.py
+++ security/lab3.py
@@ -1,12 +1,15 @@
 #! python3
 ### flake8: noqa
 
-from ctypes import c_uint32 as uint32
+from io import BytesIO
+# from ctypes import c_uint32 as uint32, c_uint
+from numpy import add, uint32
+# uint32 = int
 
 
 def random():
     _previousValue = 4
-    _module = pow(2, 10) - 1
+    _module = pow(2, 11) - 1
     _factor = pow(3, 5)
     _growth = 1
 
@@ -16,7 +19,7 @@ def random():
         yield value
 
 
-def RC5(key: bytearray):
+class RC5(object):
     W: int = 32
     R: int = 12
 
@@ -31,21 +34,22 @@ def RC5(key: bytearray):
     u: int
     c: int
 
-    def __init__(self, key):
-        self.u = self.W / 8
+    def __init__(self, key: bytearray):
+        self.u = self.W // 8
         self.b = len(key)
 
-        self.c = (self.b / self.u + 1) if (self.b % self.u > 0) else (self.b / self.u)
+        self.c = int(self.b / self.u + 1) if (self.b % self.u > 0) else int(self.b / self.u)
         self.L = [uint32(0)] * self.c  # довжина масиву - кількість байт в ключі ділити на кількість байт в слові
 
         # for (int i = b - 1; i >= 0; i--)
         for i in range(self.b - 1, -1, -1):
-            k = i / u
-            self.L[k] = RotateLeft(self.L[k], 8) + key[i]
+            k = int(i / self.u)
+            self.L[k] = self.RotateLeft(self.L[k], 8) + key[i]
 
         self.t = 2 * (self.R + 1)
-        self.S = [uint32(0)] * t
+        self.S = [uint32(0)] * self.t
         self.S[0] = self.pw
+        # self.S.insert(self.pw)
         for i in range(1, self.t):
             self.S[i] = self.S[i - 1] + self.qw
 
@@ -54,134 +58,159 @@ def RC5(key: bytearray):
         G = uint32(0)
         H = uint32(0)
 
+        # print("ASAD", type(self.S[0]))
+        # exit()
+
         a = 0
         j = 0
 
         for i in range(N):
-            G = self.S[a] = RotateLeft((self.S[a] + G + H), 3)
-            H = self.L[j] = RotateLeft((self.L[j] + G + H), int(G + H))
+            G = self.S[a] = self.RotateLeft(self.S[a] + add(G, H), 3)
+            H = self.L[j] = self.RotateLeft((self.L[j] + add(G, H)), G + H)
             a = (a + 1) % self.t
             j = (j + 1) % self.c
 
     def RotateLeft(self, a, offset):
+        # print("BBBB", a, offset, type(a), type(offset), type(self.W))
+        # a = a.value if type(a) == uint32 else a
         r1 = a << offset
-        r2 = a >> (W - offset)
+        print(self.W, offset, self.W - offset)
+        r2 = a >> (self.W - offset)
         return (r1 | r2)
 
     def RotateRight(self, a, offset):
+        # print("BBBB", type(a), type(offset), type(self.W))
+        # a = a.value if type(a) == uint32 else a
         r1 = a >> offset
-        r2 = a << (W - offset)
+        print(self.W, offset, self.W - offset)
+        r2 = a << (self.W - offset)
         return (r1 | r2)
 
-    def Encrypt_CBC_pad(self, Stream input, Stream output):
-        input.Position = 0
-        message = bytearray(input.Length)
-        input.Read(message, 0, int(input.Length))
-        v = self.u * 2 - message.Length % (2 * sizeof(uint))
-        message = message.Concat(Enumerable.Repeat(byte(v), v)).ToArray()
-        Encrypt_CBC(new MemoryStream(message), output)
+    def Encrypt_CBC_pad(self, input, output):
+        # input.Position = 0
+        input.seek(0)
+        # message = bytearray(input.Length)
+        message = input.read(len(input.getbuffer()))
+        # v = self.u * 2 - message.Length % (2 * sizeof(uint))
+        v = self.u * 2 - len(message) % (2 * 4)
+        # message = message.Concat(Enumerable.Repeat(bytes(v), v)).ToArray()
+        message = message + bytearray(v) * v
+        self.Encrypt_CBC(BytesIO(message), output)
 
-    def Decrypt_CBC_pad(self, Stream input, Stream output):
-        Stream decrypted = new MemoryStream()
-        Decrypt_CBC(input, decrypted)
-        decrypted.Position = 0
-        message = bytearray(decrypted.Length)
-        decrypted.Read(message, 0, int(decrypted.Length))
+    def Decrypt_CBC_pad(self, input, output):
+        # decrypted = new MemoryStream()
+        decrypted = BytesIO(b"")
+        self.Decrypt_CBC(input, decrypted)
+        # decrypted.Position = 0
+        decrypted.seek(0)
+        message = bytearray(len(decrypted))
+        decrypted.read(message, 0, int(decrypted.Length))
 
         if message.Length < message.Last():
-            output.Write(message, 0, message.Length)
+            output.write(message, 0, message.Length)
         else:
-            byte[] resultMessage = message.Take(message.Length - message.Last()).ToArray()
-            output.Write(resultMessage, 0, resultMessage.Length)
+            # byte[] resultMessage = message.Take(message.Length - message.Last()).ToArray()
+            resultMessage = message[:len(message) - message[-1]]
+            output.write(resultMessage, 0, len(resultMessage))
 
-    def Decrypt_CBC(self, Stream input, Stream output):
-        input.Position = 0
-        block = bytearray(self.u * 2)
-        input.Read(block, 0, self.u * 2)
-        a = uint32(0)
-        b = uint32(0)
-        a = int.from_bytes(block, "little")
-        b = int.from_bytes(block[u:], "little")
+    def Decrypt_CBC(self, input, output):
+        # input.Position = 0
+        input.seek(0)
+        # block = bytearray(self.u * 2)
+        block = input.read(0, self.u * 2)
+        a = uint32(int.from_bytes(block, "little"))
+        b = uint32(int.from_bytes(block[self.u:], "little"))
 
-        DecryptBlock_NoWrite(ref a, ref b)
+        a, b = self.DecryptBlock_NoWrite(a, b)
         while input.Position < input.Length:
-            input.Read(block, 0, u * 2)
+            input.Read(block, 0, self.u * 2)
             a = int.from_bytes(block, "little")
-            b = int.from_bytes(block[u:], "little")
-            DecryptBlock(ref a, ref b, output)
+            b = int.from_bytes(block[self.u:], "little")
+            a, b = self.DecryptBlock(a, b, output)
 
-    def DecryptBlock_NoWrite(self, ref uint a, ref uint b):
-        for i in range(R, 0, -1):
-            b = RotateRight((b - self.S[2 * i + 1]), int(a)) ^ a
-            a = RotateRight((a - self.S[2 * i]), int(b)) ^ b
+    def DecryptBlock_NoWrite(self, a, b):
+        for i in range(self.R, 0, -1):
+            b = self.RotateRight((b - self.S[2 * i + 1]), int(a)) ^ a
+            a = self.RotateRight((a - self.S[2 * i]), int(b)) ^ b
         b = b - self.S[1]
         a = a - self.S[0]
+        return a, b
 
-    def Encrypt_CBC(self, Stream input, Stream output):
+    def Encrypt_CBC(self, input, output):
         input.Position = 0
         block = bytearray(self.u * 2)
-        random = random()
+        rand = random()
 
-        a = next(random)
-        b = next(random)
-        EncryptBlock(ref a, ref b, output)
+        a = next(rand)
+        b = next(rand)
+        a, b = self.EncryptBlock(a, b, output)
         while (input.Position < input.Length):
             input.Read(block, 0, self.u * 2)
             a = int.from_bytes(block, "little")
             b = int.from_bytes(block[self.u:], "little")
-            EncryptBlock(ref a, ref b, output)
+            a, b = self.EncryptBlock(a, b, output)
 
     # Чим заповнити пробіли в останньому блоці???
-    def EncryptBlock(self, ref UInt32 a, ref UInt32 b, Stream output):
+    def EncryptBlock(self, a, b, output):
         a = a + self.S[0]
         b = b + self.S[1]
 
-        for i in range(1, self.R+1):
-            a = RotateLeft((a ^ b), int(b)) + self.S[2 * i]
-            b = RotateLeft((b ^ a), int(a)) + self.S[2 * i + 1]
-        output.Write(bytearray(a), 0, self.u)
-        output.Write(bytearray(b), 0, self.u)
+        for i in range(1, self.R + 1):
+            a = self.RotateLeft((a ^ b), b) + self.S[2 * i]
+            b = self.RotateLeft((b ^ a), a) + self.S[2 * i + 1]
+        print(self.u, a, b)
+        output.write(bytearray(a)[:self.u])
+        output.write(bytearray(b)[:self.u])
 
-    def DecryptBlock(self, ref UInt32 a, ref UInt32 b, Stream output):
-        for i in range(R, 0, -1):
-            b = RotateRight((b - self.S[2 * i + 1]), int(a)) ^ a
-            a = RotateRight((a - self.S[2 * i]), int(b)) ^ b
+        return a, b
+
+    def DecryptBlock(self, a, b, output):
+        for i in range(self.R, 0, -1):
+            b = self.RotateRight((b - self.S[2 * i + 1]), int(a)) ^ a
+            a = self.RotateRight((a - self.S[2 * i]), int(b)) ^ b
 
         b = b - self.S[1]
         a = a - self.S[0]
-        output.Write(bytearray(a), 0, self.u)
-        output.Write(bytearray(b), 0, self.u)
+        output.write(bytearray(a), 0, self.u)
+        output.write(bytearray(b), 0, self.u)
+        return a, b
 
 
 def Encrypt(password):
     key = bytearray(password, "ascii")
 
     rc5 = RC5(key)
+    print(rc5)
 
-    var rawInputStream = new FileStream("encryptionData.txt", FileMode.Open, FileAccess.Read)
-    var rawOutputStream = new FileStream("encrypted.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)
+    rawInputStream = BytesIO(b"Hello world")
+    rawOutputStream = BytesIO(b"")
+    # var rawInputStream = new FileStream("encryptionData.txt", FileMode.Open, FileAccess.Read)
+    # var rawOutputStream = new FileStream("encrypted.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)
 
     rc5.Encrypt_CBC_pad(rawInputStream, rawOutputStream)
 
-    rawInputStream.Close()
-    rawOutputStream.Close()
+    # rawInputStream.Close()
+    # rawOutputStream.Close()
 
     # Process.Start("encrypted.txt")
 
-    Console.WriteLine("The text has been encrypted successfully");
+    # Console.WriteLine("The text has been encrypted successfully");
 
 
-def Decrypt(hashKey):
-    key = bytearray(hashKey, "ascii")
+# def Decrypt(hashKey):
+#     key = bytearray(hashKey, "ascii")
 
-    rc5 = RC5(key)
-    rawInputStream = new FileStream("encrypted.txt", FileMode.Open, FileAccess.Read)
-    rawOutputStream = new FileStream("decrypted.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)
+#     rc5 = RC5(key)
+#     rawInputStream = new FileStream("encrypted.txt", FileMode.Open, FileAccess.Read)
+#     rawOutputStream = new FileStream("decrypted.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)
 
-    rc5.Decrypt_CBC_pad(rawInputStream, rawOutputStream)
-    rawInputStream.Close()
-    rawOutputStream.Close()
+#     rc5.Decrypt_CBC_pad(rawInputStream, rawOutputStream)
+#     rawInputStream.Close()
+#     rawOutputStream.Close()
 
-    # Process.Start("decrypted.txt")
+#     # Process.Start("decrypted.txt")
 
-    Console.WriteLine("The text has been decrypted successfully")
+#     Console.WriteLine("The text has been decrypted successfully")
+
+
+Encrypt("Hello world!")
