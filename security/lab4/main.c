1df603ebc404b7b0b9ecb8bf7fe6ba8d95847ad4
gebeto <slavik.nychkalo@gmail.com>
2019-12-19 01:23:48 +0200

    add openssl

---
+++ security/lab4/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
+
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
+// https://habr.com/ru/post/277935/
+
+int main() {
+
+	printf("Hello!\n");
+	// Free memory
+	// memset(rand.array, 0, M);
+
+	return 0;
+}
