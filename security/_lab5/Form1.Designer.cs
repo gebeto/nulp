c021b999c9893b94dfcbfc6bda952ca615aa2e6d
gebeto <slavik.nychkalo@gmail.com>
2019-12-19 03:54:05 +0200

    add basic 5 lab

---
+++ security/_lab5/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 13);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Згенерувати ключі";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 64);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Для тексту";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Visible = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(280, 64);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(89, 21);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Для файлу";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Visible = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 96);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Size = new System.Drawing.Size(241, 198);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(35, 166);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 26);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Створити цифровий підпис";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 13);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(233, 149);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.richTextBox2);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(280, 96);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox2.Size = new System.Drawing.Size(269, 198);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(49, 166);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 26);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Створити цифровий підпис";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(4, 42);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(261, 120);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(4, 10);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(261, 28);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Завантажити файл";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 353);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(353, 25);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 325);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Отриманий цифровий підпис:";
+            this.label1.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(366, 353);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(191, 26);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Перевірити цифровий підпис";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Visible = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 393);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Створення цифрового підпису";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ security/_lab5/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5
+{
+    public partial class Form1 : Form
+    {
+        public DSACryptoServiceProvider cryptoServiceProvider = new DSACryptoServiceProvider();
+        public DSAParameters publicKey,privateKey;
+        string fileToCreateSignature;
+        byte[] currentSignature;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                groupBox1.Visible = true;
+            }
+            else
+            {
+                groupBox1.Visible = false;
+                label1.Visible = false;
+                textBox1.Visible = false;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                groupBox2.Visible = true;
+            }
+            else
+            {
+                groupBox2.Visible = false;
+                label1.Visible = false;
+                textBox1.Visible = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            privateKey = cryptoServiceProvider.ExportParameters(true);
+            publicKey = cryptoServiceProvider.ExportParameters(false);
+            File.WriteAllText("private.xml", cryptoServiceProvider.ToXmlString(true));
+            File.WriteAllText("public.xml", cryptoServiceProvider.ToXmlString(false));
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            MessageBox.Show("Ключі згенеровано");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var textBytes = Encoding.Default.GetBytes(richTextBox1.Text);
+            cryptoServiceProvider.ImportParameters(privateKey);
+            var formatter = new DSASignatureFormatter(cryptoServiceProvider);
+            formatter.SetHashAlgorithm("SHA1");
+            var sha = SHA1.Create();
+            var signature = formatter.CreateSignature(sha.ComputeHash(textBytes));
+            currentSignature = signature;
+            textBox1.Text = BitConverter.ToString(signature).Replace("-", string.Empty);
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "TXT files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+            }
+            else
+            {
+                return;
+            }
+           
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button6.Visible = true;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Публічний Ключ";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                cryptoServiceProvider.FromXmlString(File.ReadAllText(openFileDialog1.FileName));
+            }
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Приватний ключ";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                cryptoServiceProvider.FromXmlString(File.ReadAllText(openFileDialog1.FileName));
+            }
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileToCreateSignature = openFileDialog1.FileName;
+                richTextBox2.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var signaturefile = "";
+            var openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Виберіть цифровий підпис";
+            openFileDialog1.Filter = "TXT files (*.txt)|*.txt";
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                    signaturefile = openFileDialog1.FileName;
+            } else
+            {
+                return;
+            }
+            openFileDialog1.Filter = "XML files (*.xml)|*.xml";
+            openFileDialog1.Title = "Виберіть публічний ключ";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    cryptoServiceProvider.FromXmlString(File.ReadAllText(openFileDialog1.FileName));
+                    var formatter = new DSASignatureDeformatter(cryptoServiceProvider);
+                    formatter.SetHashAlgorithm("SHA1");
+                    var sha = SHA1.Create();
+                    if (radioButton2.Checked)
+                        MessageBox.Show(cryptoServiceProvider.VerifySignature(sha.ComputeHash(File.ReadAllBytes(fileToCreateSignature)), StringToByteArray(File.ReadAllText(signaturefile).ToLower())) ? "Цифрові підписи співпадають" : "Цифрові підписи не співпадають");
+                    else
+                        MessageBox.Show(cryptoServiceProvider.VerifySignature(sha.ComputeHash(Encoding.Default.GetBytes(richTextBox1.Text)), StringToByteArray(File.ReadAllText(signaturefile).ToLower())) ? "Цифрові підписи співпадають" : "Цифрові підписи не співпадають");
+                }
+                catch(Exception)
+                {
+                    MessageBox.Show("Помилка завантаження ключа");
+                    openFileDialog1.ShowDialog();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var textBytes = File.ReadAllBytes(fileToCreateSignature);
+            cryptoServiceProvider.ImportParameters(privateKey);
+            var formatter = new DSASignatureFormatter(cryptoServiceProvider);
+            formatter.SetHashAlgorithm("SHA1");
+            var sha = SHA1.Create();
+            var signature = formatter.CreateSignature(sha.ComputeHash(textBytes));
+            currentSignature = signature;
+            textBox1.Text = BitConverter.ToString(signature).Replace("-", string.Empty);
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "TXT files (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox1.Text);
+            }
+            else
+            {
+                return;
+            }
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button6.Visible = true;
+        }
+        public static byte[] StringToByteArray(string hex)
+        {
+            return Enumerable.Range(0, hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
